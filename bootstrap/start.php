84d3adb190d9358154e89177b4afa4c1c2662905
Carlos Ignacio Pérez Sechi <secci.jr@gmail.com>
2014-09-18 23:15:27 +0200

    Solved xml version error

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,9 @@ $app = new Illuminate\Foundation\Application;
 |
  */
 
-$env = $app->detectEnvironment(array(
-
-    'local' => array('SECCI-THINK'),
-
-));
+$env = $app->detectEnvironment(function () {
+    return getenv('PROD_ENV') ? : 'local';
+});
 
 /*
 |--------------------------------------------------------------------------
